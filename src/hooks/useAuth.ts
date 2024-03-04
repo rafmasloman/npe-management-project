a7fc5e147954a729145678c4d945611640131d8f
rafmasloman <raflymasloman20@gmail.com>
2024-03-04 17:22:24 +0800

    feat: crete feature notification when login success

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -11,13 +11,18 @@ import { useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import { TOKEN_NAME } from '../constant/variables.constant';
 import { ROUTES } from '../constant/routes.constant';
+import Notifications from '../components/notifications/notification.component';
 
 export const useAuth = () => {
   const { push, replace } = useRouter();
 
   const handleSucces = (data: IApiBaseResponse<any>) => {
     if (data.statusCode !== 200) {
-      console.log('login gagal');
+      Notifications({
+        message: 'Email atau Password salah',
+        title: 'Login Gagal',
+        status: 'FAILED',
+      });
     }
 
     __setBrowserAuthCookie(TOKEN_NAME, data.data?.token);
@@ -25,6 +30,12 @@ export const useAuth = () => {
     const token = __getBrowserAuthCookie(TOKEN_NAME);
 
     if (token?.length! > 0) {
+      Notifications({
+        message: 'Berhasil Login',
+        title: 'Login Berhasil',
+        status: 'SUCCESS',
+      });
+
       push('/dashboard');
     }
   };
