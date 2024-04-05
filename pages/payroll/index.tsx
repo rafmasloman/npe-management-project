190167e128d23ea6447b5007e70a09aa5dd44469
rafmasloman <raflymasloman20@gmail.com>
2024-04-06 01:53:17 +0800

    feat: add no data card to payroll

--- pages/payroll/index.tsx
+++ pages/payroll/index.tsx
@@ -1,12 +1,15 @@
 import { ICAlert } from '@/src/assets/icons/alert_delete.icon';
+import { ICPayroll } from '@/src/assets/icons/nav-icon/payroll.icon';
 import SEO from '@/src/components/SEO/seo.component';
 import ButtonNavigate from '@/src/components/button/button-link.component';
+import NoDataCard from '@/src/components/card/no_data-card.component';
 import HeaderPage from '@/src/components/header/header-page.component';
 import Table from '@/src/components/table/table.component';
 import { COLORS } from '@/src/constant/colors.constant';
 import { useDeletePayroll } from '@/src/hooks/payroll/useDeletePayrollMutation';
 import { useGetPayrollQuery } from '@/src/hooks/payroll/useGetPayrollsQuery';
 import { useModal } from '@/src/hooks/useModal';
+import TableLayout from '@/src/layouts/form/table.layout';
 import MainLayout from '@/src/layouts/main.layout';
 import { getCurrentPage, getCurrentRole } from '@/src/utils/page.util';
 import {
@@ -28,13 +31,14 @@ import { useDisclosure } from '@mantine/hooks';
 import { IconCircleFilled, IconPlus, IconX } from '@tabler/icons-react';
 import { IconPencilCode, IconTrash } from '@tabler/icons-react';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AdminPayroll = () => {
   const { pathname } = useRouter();
 
   const { data: payrolls } = useGetPayrollQuery();
   const { mutate: deletePayroll } = useDeletePayroll();
+  const [isPayrollAvaiable, setIsPayrollAvaiable] = useState(false);
 
   const { opened, open, close, itemId, handleConfirm } = useModal();
 
@@ -52,6 +56,12 @@ const AdminPayroll = () => {
     deletePayroll(itemId);
     close();
   };
+
+  useEffect(() => {
+    if (payrolls?.data?.length > 0) {
+      setIsPayrollAvaiable(!isPayrollAvaiable);
+    }
+  }, [payrolls, isPayrollAvaiable]);
 
   return (
     <MainLayout>
@@ -124,184 +134,229 @@ const AdminPayroll = () => {
           role={getCurrentRole(pathname)}
         />
 
-        {/* <Stack className="w-full">
-          <Grid className="" gutter={120}>
-            <Grid.Col span={1}>
-              <h4>No.</h4>
-            </Grid.Col>
-
-            <Grid.Col span={3}>
-              <h4>Member</h4>
-            </Grid.Col>
-
-            <Grid.Col span={3}>
-              <h4>Position</h4>
-            </Grid.Col>
-
-            <Grid.Col span={1}>
-              <h4>Provider</h4>
-            </Grid.Col>
-
-            <Grid.Col span={1}>
-              <h4>Status</h4>
-            </Grid.Col>
-
-            <Grid.Col span={2}>
-              <h4>Project</h4>
-            </Grid.Col>
-          </Grid>
-
-          <Grid className="" gutter={120}>
-            <Grid.Col span={1}>
-              <p>1</p>
-            </Grid.Col>
-
-            <Grid.Col span={3}>
-              <p>Rafly Masloman</p>
-            </Grid.Col>
-
-            <Grid.Col span={3}>
-              <p>Frontend Developer</p>
-            </Grid.Col>
-
-            <Grid.Col span={1}>
-              <p>BRI</p>
-            </Grid.Col>
-
-            <Grid.Col span={1}>
-              <p>Unpaid</p>
-            </Grid.Col>
-
-            <Grid.Col span={2}>
-              <div className="flex justify-center items-center">
-                <Avatar />
-                <p>Kartjis</p>
-              </div>
-            </Grid.Col>
-          </Grid>
-        </Stack> */}
-
         <Space h={50} />
 
-        <div className="w-full bg-white py-8 rounded-2xl border border-solid border-gray-200">
-          <Group
-            align="center"
-            position="apart"
-            className="border-b-2 border-0 border-solid border-gray-300 px-8 pb-5"
-          >
-            <Text className="text-blue-950 text-lg font-semibold ">
-              {payrolls?.data?.length} Payroll
-            </Text>
+        <TableLayout
+          layoutTitle={`${payrolls?.data?.length} Payroll`}
+          addUrl="/add-client"
+          icon={<ICPayroll width={30} height={30} />}
+        >
+          {!isPayrollAvaiable ? (
+            <Box className="flex justify-center">
+              <NoDataCard
+                icon={<ICPayroll width={50} height={50} />}
+                description="Untuk memberikan persen keuntungan kepada anggota tim yang terlibat, tambahkan payroll terlebih dahulu"
+                title="Payroll"
+              >
+                <ButtonNavigate
+                  icon={<IconPlus />}
+                  url={`/${getCurrentPage(pathname)}/add-payroll`}
+                >
+                  Tambah Payroll
+                </ButtonNavigate>
+              </NoDataCard>
+            </Box>
+          ) : (
+            <>
+              <Table
+                tableHead={tableHead}
+                tableRow={payrolls?.data?.map((payroll: any) => {
+                  return (
+                    <tr key={payroll.id} className="">
+                      <td className=" md:w-1/5 ">
+                        <div className="flex flex-row items-center gap-5">
+                          <Avatar
+                            radius={'xl'}
+                            size={36}
+                            src={
+                              !payroll.member.profilePicture
+                                ? ''
+                                : `${process.env.NEXT_PUBLIC_API_DOWNLOAD_FILES_URL}/members/${payroll.member?.profilePicture}`
+                            }
+                          />
 
-            <ButtonNavigate
-              icon={<IconPlus />}
-              url={`/${getCurrentRole(pathname)}/add-payroll`}
-            >
-              Tambah Payroll
-            </ButtonNavigate>
-          </Group>
+                          <Text className="text-left">
+                            {`${payroll.member?.user?.firstname} ${payroll.member?.user?.lastname}`}
+                          </Text>
+                        </div>
+                      </td>
+                      <td className="md:w-1/6  ">{`${payroll.member?.position}`}</td>
+                      <td className="">{payroll.percent} %</td>
+                      <td className=" ">{payroll.salary}</td>
+                      <td className="md:w-1/6">
+                        <div className="flex items-center gap-2.5">
+                          <IconCircleFilled
+                            size={10}
+                            style={{
+                              color:
+                                payroll.payrollStatus === 'PAID'
+                                  ? COLORS.SUCCESS
+                                  : COLORS.ON_PROGRESS,
+                            }}
+                          />
+                          {payroll.payrollStatus === 'PAID'
+                            ? 'Sudah Dibayar'
+                            : 'Belum Dibayar'}
+                        </div>
+                      </td>
+                      <td className="md:w-1/6 ">
+                        <div className="flex flex-row items-center gap-2.5">
+                          <Avatar
+                            radius={'xl'}
+                            size={20}
+                            className="w-2 h-fit"
+                            src={
+                              !payroll.project.projectIcon
+                                ? ''
+                                : `${process.env.NEXT_PUBLIC_API_DOWNLOAD_FILES_URL}/projects/${payroll.project?.projectIcon}`
+                            }
+                          />
 
-          <Space h={35} />
+                          <Text className="text-left">
+                            {`${payroll.project?.projectName}`}
+                          </Text>
+                        </div>
+                      </td>
+                      <td className=" ">
+                        <Group position="left">
+                          <ActionIcon
+                            variant="outline"
+                            radius={'xl'}
+                            color={'red'}
+                            c={COLORS.DANGER}
+                            opacity={'0.7'}
+                            size={'lg'}
+                            onClick={() => handleConfirm(payroll.id)}
+                          >
+                            <IconTrash size={'1.125rem'} />
+                          </ActionIcon>
 
-          <div className="px-8 overflow-x-scroll lg:overflow-x-hidden">
-            <Table
-              tableHead={tableHead}
-              tableRow={payrolls?.data?.map((payroll: any) => {
-                return (
-                  <tr key={payroll.id} className="">
-                    <td className=" md:w-1/5 ">
-                      <div className="flex flex-row items-center gap-5">
-                        <Avatar
-                          radius={'xl'}
-                          size={36}
-                          src={
-                            !payroll.member.profilePicture
-                              ? ''
-                              : `${process.env.NEXT_PUBLIC_API_DOWNLOAD_FILES_URL}/members/${payroll.member?.profilePicture}`
-                          }
-                        />
+                          <ActionIcon
+                            variant="outline"
+                            radius={'xl'}
+                            color={'gray'}
+                            c={COLORS.SECONDARY}
+                            opacity={'0.7'}
+                            size={'lg'}
+                            // onClick={() => {
+                            //   replace(`/admin/payroll-management/${user.id}/edit-user`);
+                            // }}
+                          >
+                            <IconPencilCode size={'1.125rem'} />
+                          </ActionIcon>
+                        </Group>
+                      </td>
+                    </tr>
+                  );
+                })}
+              />
 
-                        <Text className="text-left">
-                          {`${payroll.member?.user?.firstname} ${payroll.member?.user?.lastname}`}
-                        </Text>
-                      </div>
-                    </td>
-                    <td className="md:w-1/6  ">{`${payroll.member?.position}`}</td>
-                    <td className="">{payroll.percent} %</td>
-                    <td className=" ">{payroll.salary}</td>
-                    <td className="md:w-1/6">
-                      <div className="flex items-center gap-2.5">
-                        <IconCircleFilled
-                          size={10}
-                          style={{
-                            color:
-                              payroll.payrollStatus === 'PAID'
-                                ? COLORS.SUCCESS
-                                : COLORS.ON_PROGRESS,
-                          }}
-                        />
-                        {payroll.payrollStatus === 'PAID'
-                          ? 'Sudah Dibayar'
-                          : 'Belum Dibayar'}
-                      </div>
-                    </td>
-                    <td className="md:w-1/6 ">
-                      <div className="flex flex-row items-center gap-2.5">
-                        <Avatar
-                          radius={'xl'}
-                          size={20}
-                          className="w-2 h-fit"
-                          src={
-                            !payroll.project.projectIcon
-                              ? ''
-                              : `${process.env.NEXT_PUBLIC_API_DOWNLOAD_FILES_URL}/projects/${payroll.project?.projectIcon}`
-                          }
-                        />
+              <Space h={30} />
 
-                        <Text className="text-left">
-                          {`${payroll.project?.projectName}`}
-                        </Text>
-                      </div>
-                    </td>
-                    <td className=" ">
-                      <Group position="left">
-                        <ActionIcon
-                          variant="outline"
-                          radius={'xl'}
-                          color={'red'}
-                          c={COLORS.DANGER}
-                          opacity={'0.7'}
-                          size={'lg'}
-                          onClick={() => handleConfirm(payroll.id)}
-                        >
-                          <IconTrash size={'1.125rem'} />
-                        </ActionIcon>
+              <div className="flex justify-center ">
+                <Pagination total={10} />
+              </div>
+              <Table
+                tableHead={tableHead}
+                tableRow={payrolls?.data?.map((payroll: any) => {
+                  return (
+                    <tr key={payroll.id} className="">
+                      <td className=" md:w-1/5 ">
+                        <div className="flex flex-row items-center gap-5">
+                          <Avatar
+                            radius={'xl'}
+                            size={36}
+                            src={
+                              !payroll.member.profilePicture
+                                ? ''
+                                : `${process.env.NEXT_PUBLIC_API_DOWNLOAD_FILES_URL}/members/${payroll.member?.profilePicture}`
+                            }
+                          />
 
-                        <ActionIcon
-                          variant="outline"
-                          radius={'xl'}
-                          color={'gray'}
-                          c={COLORS.SECONDARY}
-                          opacity={'0.7'}
-                          size={'lg'}
-                          // onClick={() => {
-                          //   replace(`/admin/payroll-management/${user.id}/edit-user`);
-                          // }}
-                        >
-                          <IconPencilCode size={'1.125rem'} />
-                        </ActionIcon>
-                      </Group>
-                    </td>
-                  </tr>
-                );
-              })}
-            />
-          </div>
+                          <Text className="text-left">
+                            {`${payroll.member?.user?.firstname} ${payroll.member?.user?.lastname}`}
+                          </Text>
+                        </div>
+                      </td>
+                      <td className="md:w-1/6  ">{`${payroll.member?.position}`}</td>
+                      <td className="">{payroll.percent} %</td>
+                      <td className=" ">{payroll.salary}</td>
+                      <td className="md:w-1/6">
+                        <div className="flex items-center gap-2.5">
+                          <IconCircleFilled
+                            size={10}
+                            style={{
+                              color:
+                                payroll.payrollStatus === 'PAID'
+                                  ? COLORS.SUCCESS
+                                  : COLORS.ON_PROGRESS,
+                            }}
+                          />
+                          {payroll.payrollStatus === 'PAID'
+                            ? 'Sudah Dibayar'
+                            : 'Belum Dibayar'}
+                        </div>
+                      </td>
+                      <td className="md:w-1/6 ">
+                        <div className="flex flex-row items-center gap-2.5">
+                          <Avatar
+                            radius={'xl'}
+                            size={20}
+                            className="w-2 h-fit"
+                            src={
+                              !payroll.project.projectIcon
+                                ? ''
+                                : `${process.env.NEXT_PUBLIC_API_DOWNLOAD_FILES_URL}/projects/${payroll.project?.projectIcon}`
+                            }
+                          />
 
-          <div className="flex justify-center ">
-            <Pagination total={10} />
-          </div>
-        </div>
+                          <Text className="text-left">
+                            {`${payroll.project?.projectName}`}
+                          </Text>
+                        </div>
+                      </td>
+                      <td className=" ">
+                        <Group position="left">
+                          <ActionIcon
+                            variant="outline"
+                            radius={'xl'}
+                            color={'red'}
+                            c={COLORS.DANGER}
+                            opacity={'0.7'}
+                            size={'lg'}
+                            onClick={() => handleConfirm(payroll.id)}
+                          >
+                            <IconTrash size={'1.125rem'} />
+                          </ActionIcon>
+
+                          <ActionIcon
+                            variant="outline"
+                            radius={'xl'}
+                            color={'gray'}
+                            c={COLORS.SECONDARY}
+                            opacity={'0.7'}
+                            size={'lg'}
+                            // onClick={() => {
+                            //   replace(`/admin/payroll-management/${user.id}/edit-user`);
+                            // }}
+                          >
+                            <IconPencilCode size={'1.125rem'} />
+                          </ActionIcon>
+                        </Group>
+                      </td>
+                    </tr>
+                  );
+                })}
+              />
+
+              <Space h={30} />
+
+              <div className="flex justify-center ">
+                <Pagination total={10} />
+              </div>
+            </>
+          )}
+        </TableLayout>
       </Container>
     </MainLayout>
   );
